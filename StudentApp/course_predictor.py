f2767bca4788695e4c2f9bc0bd176dcb1a0b64f6
Kadirjpn <124694810+Kadirjpn@users.noreply.github.com>
2024-10-03 19:43:56 +0800

    Update course_predictor.py

--- StudentApp/course_predictor.py
+++ StudentApp/course_predictor.py
@@ -16,10 +16,8 @@ class NpEncoder(json.JSONEncoder):
         return super(NpEncoder, self).default(obj)
         
 cm_path = Path(__file__).parents[1] / 'HajiProject/Data/class_mapping.txt'
-
 # Function to save updated class_mappings back to class_mapping.txt
 def save_updated_mappings(class_mappings):
-    
     with open('cm_path', 'w') as f:
         modified_mappings = {
             k: {str(k2): v2 for k2, v2 in v.items()} if isinstance(v, dict) else v 
@@ -28,7 +26,7 @@ def save_updated_mappings(class_mappings):
         f.write(json.dumps(modified_mappings, cls=NpEncoder))
         
 ct_path = Path(__file__).parents[1] / 'HajiProject/Data/course_taken.txt'
-
+cm_path = Path(__file__).parents[1] / 'HajiProject/Data/class_mapping.txt'
 # Function to load predefined course_taken and class_mappings from text files
 def load_predefined_mappings():
     # Load course_taken mapping from course_taken.txt
